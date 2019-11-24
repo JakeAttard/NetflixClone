734cc290fb7e27f399c5b0ed8ba337713cdd1c1d
Jake Attard <jakeattard18@gmail.com>
2019-11-24 20:27:46 +1000

    Created the header section for the register page and styled.

--- register.php
+++ register.php
@@ -1,12 +1,25 @@
+<?php 
+    if(isset($_POST["submitButton"])) {
+        echo "Form was submitted";
+    }
+?>
+
 <!DOCTYPE html>
 <html>
+
     <head>
         <title>Welcome to NetflixClone</title>
         <link rel="stylesheet" type="text/css" href="assets/style/style.css"/>
     </head>
+
     <body>
         <div class="signInContainer">
             <div class="column">
+                <div class="header">
+                    <h3>Sign Up</h3>
+                    <span>to continue to NetflixClone</span>
+                </div>
+
                 <form method="POST">
                     <input type="text" name="firstName" placeholder="First name" required>
                     <input type="text" name="lastName" placeholder="Last name" required>
@@ -15,7 +28,7 @@
                     <input type="email" name="email2" placeholder="Confirm Email" required>
                     <input type="password" name="password" placeholder="Password" required>
                     <input type="password" name="password2" placeholder="Confirm Password" required>
-                    <input type="submit" name="submitPassword" value="SUBMIT">
+                    <input type="submit" name="submitButton" value="SUBMIT">
                 </form>
             </div>
         </div>
