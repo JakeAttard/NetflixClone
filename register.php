d4e0e3bb25e3b520643f0e7dc93d8d91f934e924
Jake Attard <jakeattard18@gmail.com>
2019-11-24 20:03:13 +1000

    Creating the register page.

---
+++ register.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome to NetflixClone</title>
+        <link rel="stylesheet" type="text/css" href="assets/style/style.css"/>
+    </head>
+    <body>
+
+    </body>
+</html>
