9d3c80713ae831f29f731f35aecb79076fcd595d
Jake Attard <jakeattard18@gmail.com>
2019-11-24 20:13:27 +1000

    Created the register form input fields.

--- register.php
+++ register.php
@@ -7,7 +7,16 @@
     <body>
         <div class="signInContainer">
             <div class="column">
-
+                <form method="POST">
+                    <input type="text" name="firstName" placeholder="First name" required>
+                    <input type="text" name="lastName" placeholder="Last name" required>
+                    <input type="text" name="username" placeholder="Username" required>
+                    <input type="email" name="email" placeholder="Email" required>
+                    <input type="email" name="email2" placeholder="Confirm Email" required>
+                    <input type="password" name="password" placeholder="Password" required>
+                    <input type="password" name="password2" placeholder="Confirm Password" required>
+                    <input type="submit" name="submitPassword" value="SUBMIT">
+                </form>
             </div>
         </div>
     </body>
