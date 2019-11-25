30fbea9e9a0b91df4367601ba1a68589e90f8ff0
Jake Attard <jakeattard18@gmail.com>
2019-11-25 20:28:13 +1000

    Connecting to the database in the config.php file.

--- includes/config.php
+++ includes/config.php
@@ -4,4 +4,12 @@ ob_start();
 session_start();
 
 date_default_timezone_set("Australia/Brisbane");
+
+try {
+    $con = new PDO("mysql:dbname=netflixclone;host=localhost", "root", "");
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+}
+catch (PDOException $e) {
+    exit("Connection failed: " . $e->getMessage());
+}
 ?>
--- register.php
+++ register.php
@@ -1,4 +1,5 @@
 <?php
+    require_once("includes/config.php");
     require_once("includes/classes/FormSanitizer.php");
 
     if(isset($_POST["submitButton"])) {
