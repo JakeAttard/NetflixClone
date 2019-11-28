d55a76b3e53e046729392e25ec8904b6c9679c9c
Jake Attard <jakeattard18@gmail.com>
2019-11-28 21:56:12 +1000

    Looped over each entity and outputted it in html, styled the entity previews and category sections.

--- includes/classes/CategoryContainers.php
+++ includes/classes/CategoryContainers.php
@@ -27,9 +27,35 @@ class CategoryContainers {
         $categoryId = $sqlData["id"];
         $title = $title == null ? $sqlData["name"] : $title;
 
+        if($tvShows && $movies) {
+            $entities = EntityProvider::getEntities($this->con, $categoryId, 30);
+        } else if($tvShows) {
+            // Get tv show entities
+        } else {
+            // Get movie entities
+        }
 
+        if(sizeof($entities) == 0) {
+            return;
+        }
 
-        return $title . "<br>";
+        $entitiesHtml = "";
+
+        $previewProvider = new PreviewProvider($this->con, $this->username);
+
+        foreach($entities as $entity) {
+            $entitiesHtml .= $previewProvider->createEntityPreviewSquare($entity);
+        }
+
+        return "<div class='category'>
+                    <a href='category.php?id=$categoryId'>
+                        <h3>$title</h3>
+                    </a>
+                    
+                    <div class='entities'>
+                        $entitiesHtml
+                    </div>
+                </div>";
     }
 }
 
--- includes/classes/PreviewProvider.php
+++ includes/classes/PreviewProvider.php
@@ -43,6 +43,18 @@ class PreviewProvider {
                 </div>";
     }
 
+    public function createEntityPreviewSquare($entity) {
+        $id = $entity->getId();
+        $thumbnail = $entity->getThumbnail();
+        $name = $entity->getName();
+
+        return "<a href='entity.php?id=$id'>
+                    <div class='previewContainer small'>
+                        <img src='$thumbnail' title='$name'>
+                    </div>
+                </a>";
+    }
+
     private function getRandomEntity() {
         $entity = EntityProvider::getEntities($this->con, null, 1);
         return $entity[0];
