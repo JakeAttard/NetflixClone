21ccf97182a3ca570fc4e9a88aa700032514f2e5
Jake Attard <jakeattard18@gmail.com>
2019-11-28 18:31:02 +1000

    Created the PreviewProvider Class and getting a random entity from within the database table.

---
+++ includes/classes/PreviewProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+
+class PreviewProvider {
+    private $con;
+    private $username;
+
+    public function __construct($con, $username) {
+        $this->con = $con;
+        $this->username = $username;
+    }
+
+    public function createPreviewVideo($entity) {
+        if ($entity == null) {
+           $entity = $this->getRandomEntity();
+        }
+    }
+
+    private function getRandomEntity() {
+        $query = $this->con->prepare("SELECT * FROM entities ORDER BY RAND() LIMIT 1");
+        $query->execute();
+
+        $row = $query->fetch(PDO::FETCH_ASSOC);
+        echo $row["name"];
+    }
+}
+
+?>
