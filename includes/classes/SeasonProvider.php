9ed5b5734c503a9f13423fced8baf9977c93c228
Jake Attard <jakeattard18@gmail.com>
2019-11-29 22:32:06 +1000

    Outputting the season numbers, video information such as  episodes in a season, titles, thumbnails and description.

--- includes/classes/SeasonProvider.php
+++ includes/classes/SeasonProvider.php
@@ -12,6 +12,50 @@ class SeasonProvider {
 
     public function create($entity) {
         $seasons = $entity->getSeasons();
+
+        if(sizeof($seasons) == 0) {
+            return;
+        }
+
+        $seasonsHtml = "";
+
+        foreach ($seasons as $season) {
+            $seasonNumber = $season->getSeasonNumber();
+
+            $videosHtml = "";
+            foreach ($season->getVideos() as $video) {
+                $videosHtml .= $this->createVideoSquare($video);
+            }
+
+            $seasonsHtml .= "<div class='season'>
+                                <h3>Season $seasonNumber</h3>
+                                <div class='videos'>
+                                    $videosHtml
+                                </div>
+                            </div>";
+        }
+
+        return $seasonsHtml;
+    }
+
+    private function createVideoSquare($video) {
+        $id = $video->getId();
+        $thumbnail = $video->getThumbnail();
+        $title = $video->getTitle();
+        $description = $video->getDescription();
+        $episodeNumber = $video->getEpisodeNumber();
+
+        return "<a href='watch.php?id=$id'>
+                    <div class='episodeContainer'>
+                        <div class='content'>
+                            <img src='$thumbnail'>
+                            <div class='videoInfo'>
+                                <h4>$title</h4>
+                                <span>$description</span>
+                            </div>
+                        </div>
+                    </div>
+                </a>";
     }
 }
 
