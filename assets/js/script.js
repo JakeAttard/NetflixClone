03105dcc2072454da64fd5ce77197aaae0481ce5
Jake Attard <jakeattard18@gmail.com>
2019-12-02 23:55:07 +1000

    Hiding the navbar on the watch.php page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+$(document).scroll(function() {
+    var isScrolled = $(this).scrollTop() > $(".topBar").height()
+    $(".topBar").toggleClass("scrolled", isScrolled);
+})
+
 function volumeToggle(button) {
     var muted = $(".previewVideo").prop("muted");
     $(".previewVideo").prop("muted", !muted);
--- includes/header.php
+++ includes/header.php
@@ -32,25 +32,8 @@
     <body>
         <div class="wrapper">
 
-        <div class="topBar">
-            <div class="logoContainer">
-                <a href="index.php">
-                    <img src="assets/images/netflixlogo.jpg" alt="Netflix Logo">
-                </a>
-            </div>
-
-            <ul class="navLinks">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="shows.php">TV Shows</a></li>
-                <li><a href="movies.php">Movies</a></li>
-            </ul>
-
-            <div class="rightItems">
-                <a href="search.php">
-                    <i class="fas fa-search"></i>
-                </a>
-                <a href="profile.php">
-                    <i class="fas fa-user"></i>
-                </a>
-            </div>
-        </div>
+<?php
+    if(!isset($hideNav)) {
+        include_once("includes/navBar.php");
+    }
+?>
