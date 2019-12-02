9793c2dc1cd7f49fd6a96266571156625a422a95
Jake Attard <jakeattard18@gmail.com>
2019-12-02 23:00:34 +1000

    Subtitle for the video preview on the homepage added.

--- includes/classes/PreviewProvider.php
+++ includes/classes/PreviewProvider.php
@@ -20,9 +20,11 @@ class PreviewProvider {
         $preview = $entity->getPreview();
         $thumbnail = $entity->getThumbnail();
 
-        // TODO: ADD SUBTITLE
-
         $videoId = VideoProvider::getEntityVideoForUser($this->con, $id, $this->username);
+        $video = new Video($this->con, $videoId);
+
+        $seasonEpisode = $video->getSeasonAndEpisode();
+        $subHeading = $video->isMovie() ? "" : "<h4>$seasonEpisode</h4>";
 
         return "<div class='previewContainer'>
                     <img src='$thumbnail' class='previewImage' hidden>
@@ -34,7 +36,7 @@ class PreviewProvider {
                     <div class='previewOverlay'>
                         <div class='mainDetails'>
                             <h3>$name</h3>
-                            
+                            $subHeading
                             <div class='butttons'>
                                 <button onclick='watchVideo($videoId);'><i class='fas fa-play'></i> Play</button>
                                 <button onclick='volumeToggle(this)'><i class='fas fa-volume-mute'></i></button>
