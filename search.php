746c3461ab70231500c8636626abbbb1e0776a68
Jake Attard <jakeattard18@gmail.com>
2019-12-03 23:07:11 +1000

    Search page created and input text styled.

---
+++ search.php
@@ -0,0 +1,11 @@
+<?php
+    include_once("includes/header.php");
+?>
+
+<div class="textboxContainer">
+    <input type="text" class="searchInput" placeholder="Search for something">
+</div>
+
+<div class="results">
+
+</div>
