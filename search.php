3d55f3522a6bb52b150cb81cf80a4454440a433f
Jake Attard <jakeattard18@gmail.com>
2019-12-04 22:27:40 +1000

    Search results now working.

--- search.php
+++ search.php
@@ -9,3 +9,27 @@
 <div class="results">
 
 </div>
+
+<script>
+    $(function () {
+        var username = '<?php echo $userLoggedIn; ?>';
+        var timer;
+
+        $(".searchInput").keyup(function() {
+            clearTimeout(timer);
+
+            timer = setTimeout(function() {
+                var val = $(".searchInput").val();
+
+                if(val != "") {
+                    $.post("ajax/getSearchResults.php", { term: val, username: username }, function(data) {
+                        $(".results").html(data);
+                    })
+                } else {
+                    $(".results").html("");
+                }
+
+            }, 500);
+        })
+    })
+</script>
