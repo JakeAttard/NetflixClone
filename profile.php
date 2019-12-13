a93d58376604ff1d8b53b9dd0cfbfbe163045d6c
Jake Attard <jakeattard18@gmail.com>
2019-12-13 21:04:32 +1000

    Profile page input boxes now prefilling with user correct details.

--- profile.php
+++ profile.php
@@ -6,9 +6,18 @@
     <div class="formSection">
         <form method="POST">
             <h2>User Details</h2>
-            <input type="text" name="firstName" placeholder="First Name">
-            <input type="text" name="lastName" placeholder="Last Name">
-            <input type="email" name="email" placeholder="Email">
+
+            <?php
+                $user = new User($con, $userLoggedIn);
+
+                $firstName = isset($_POST["firstName"]) ? $_POST["firstName"] : $user->getFirstName();
+                $lastName = isset($_POST["lastName"]) ? $_POST["lastName"] : $user->getLastName();
+                $email = isset($_POST["email"]) ? $_POST["email"] : $user->getEmail();
+            ?>
+
+            <input type="text" name="firstName" placeholder="First Name" value="<?php echo $firstName; ?>">
+            <input type="text" name="lastName" placeholder="Last Name" value="<?php echo $lastName; ?>">
+            <input type="email" name="email" placeholder="Email" value="<?php echo $email; ?>">
 
             <input type="submit" name="saveDetailsButton" value="Save">
         </form>
